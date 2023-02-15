e6ba9567598fb44ea0084a1b5235b12a9037f3d5
--local <--local>
2023-02-15 12:40:33 +1100

    need filter function

--- fastapi_filter/contrib/geoalchemy/filter.py
+++ fastapi_filter/contrib/geoalchemy/filter.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from enum import Enum
-from typing import List
+from typing import List, Union
 
 from pydantic import validator
 from geoalchemy2 import functions
-
+from sqlalchemy.orm import Query
+from sqlalchemy.sql.selectable import Select
+from sqlalchemy import or_
 from ..sqlalchemy.filter import Filter as SQLAlchemyFilter, _orm_operator_transformer as _sqla_orm_operator_transformer
 
 
@@ -62,3 +64,27 @@ class Filter(SQLAlchemyFilter):
         ) and isinstance(value, str):
             return [field.type_(v) for v in value.split(",")]
         return value
+
+    def filter(self, query: Union[Query, Select]):
+        for field_name, value in self.filtering_fields:
+            field_value = getattr(self, field_name)
+            if isinstance(field_value, Filter):
+                query = field_value.filter(query)
+            else:
+                if "__" in field_name:
+                    field_name, operator = field_name.split("__")
+                    operator, value = _orm_operator_transformer[operator](value)
+                else:
+                    operator = "__eq__"
+
+                if field_name == self.Constants.search_field_name and hasattr(self.Constants, "search_model_fields"):
+
+                    def search_filter(field):
+                        return getattr(self.Constants.model, field).ilike("%" + value + "%")
+
+                    query = query.filter(or_(*list(map(search_filter, self.Constants.search_model_fields))))
+                else:
+                    model_field = getattr(self.Constants.model, field_name)
+                    query = query.filter(getattr(model_field, operator)(value))
+
+        return query
